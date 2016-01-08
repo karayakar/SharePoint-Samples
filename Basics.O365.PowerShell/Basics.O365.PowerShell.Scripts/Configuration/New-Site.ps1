87c2bda183045d10c11ed0b6406cefb4dfc302c4
Edouard Shaar <edouard.shaar@gsoft.com>
2016-01-08 10:44:21 -0500

    Added the Basics.O365.PowerShell Sample

---
+++ Basics.O365.PowerShell/Basics.O365.PowerShell.Scripts/Configuration/New-Site.ps1
@@ -0,0 +1,49 @@
+﻿<#	
+	Remote provisioning engine for a SharePoint Online site collection. XML definition used is based on PnP Schema definition.
+
+	PREREQUISITE: You must install the PnP Cmdlet in order for this script to work properly.
+				  https://github.com/OfficeDev/PnP-PowerShell/blob/master/Binaries/PnPPowerShellCommands16.msi?raw=true
+#>
+Param
+(
+	[Parameter(Mandatory=$true)]
+	[xml]$Tokens
+)
+
+Try
+{
+	# Connecto the the Admin tenant
+	$AdminTenantUrl = $Tokens.Tokens.AdminTenantUrl;
+	Write-Host "Trying to connect to $AdminTenantUrl..." -ForeGroundColor Yellow -NoNewLine
+	Connect-SPOnline -Url $AdminTenantUrl
+	Write-Host "Success!" -ForegroundColor Green
+
+	Try 
+	{
+		# Get current user
+		$context = Get-SPOContext
+		$currentUser = $context.Credentials.UserName
+
+		# Get site collection information
+		$url = $Tokens.Tokens.SiteCollection.Url
+		$title = $Tokens.Tokens.SiteCollection.Title
+
+		$existingSite = Get-SPOTenantSite -Url $url -ErrorAction SilentlyContinue
+		if($existingSite -eq $null) {
+			Write-Host "Creating '$url'..." -ForeGroundColor Yellow -NoNewline
+			New-SPOTenantSite -Title $title -Url $url -Lcid 1033 -Owner $currentUser -TimeZone 10 -Template STS#0 -Wait -RemoveDeletedSite
+			Write-Host "Success!" -ForegroundColor Green
+		} else {
+			Write-Host "Site collection at url '$url' already exists." -ForeGroundColor Yellow
+		}		
+	}
+	Catch
+	{
+		Write-Error $_.Exception.Message
+	}
+}
+Catch
+{
+	Write-Host "Failed!" -ForegroundColor Red
+	Write-Error $_.Exception.Message
+}
---
+++ Basics.O365.PowerShell/Basics.O365.PowerShell.Scripts/Configuration/Provision-Site.ps1
@@ -0,0 +1,37 @@
+﻿<#	
+	Remote provisioning engine for a SharePoint Online site collection. XML definition used is based on PnP Schema definition.
+
+	PREREQUISITE: You must install the PnP Cmdlet in order for this script to work properly.
+				  https://github.com/OfficeDev/PnP-PowerShell/blob/master/Binaries/PnPPowerShellCommands16.msi?raw=true
+#>
+Param
+(
+	[Parameter(Mandatory=$true)]
+	[xml]$Tokens
+)
+
+Try
+{
+	# Connect to the site collection
+	$SiteCollectionUrl = $Tokens.Tokens.SiteCollection.Url;
+	Write-Host "Trying to connect to $SiteCollectionUrl..." -ForeGroundColor Yellow -NoNewLine
+	Connect-SPOnline -Url $SiteCollectionUrl
+	Write-Host "Success!" -ForegroundColor Green
+
+	Try
+	{	
+		Write-Host "Provisioning files..." -ForeGroundColor Yellow -NoNewline
+		Apply-SPOProvisioningTemplate -Path ".\Templates\files.xml"
+		Write-Host "Success!" -ForeGroundColor Green
+	}
+	Catch
+	{
+		Write-Error $_.Exception.Message
+	}
+}
+Catch
+{
+	Write-Host "Failed!" -ForegroundColor Red
+	Write-Error $_.Exception.Message
+}
+
